372329474cee05db8c286da0736e7e7e43d28ecf
CS <badquark@protonmail.ch>
2022-03-02 21:20:07 -0500

    Minted NFT with solidity successfully

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/8MktGBJcuxqCmA7WdIIDh0p0XE5OQIem',
+      accounts: ['49055e6e46bdcbf14bd10447c7f608969f959a1e267a21f1d060c3941f1534fb'],
+    },
+  },
 };
